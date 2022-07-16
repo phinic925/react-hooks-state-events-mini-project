c40198de6373bcd6d7ec71c5082f34b95a465e2a
phinic925 <phinicnyangate480@gmail.com>
2022-07-16 20:55:02 +0100

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,30 @@ import TaskList from "./TaskList";
 import { CATEGORIES, TASKS } from "../data";
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
+const [tasks, setTasks]=useState(TASKS);
+  const [category, setCategory]=useState("All")
+  function handleAddTask(newTask){
+    setTasks([...tasks, newTask]);
+  }
+  function handleDeleteTask(deletedTaskText){
+    setTasks(tasks.filter((task)=>task.text !==deletedTaskText));
+  }
+  const visisbleTasks=tasks.filter(
+    (task)=>category==="All" || task. category=== category
+  );
+
 
 function App() {
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+      categories={CATEGORIES} selectedCategory={category}   onSelectCategory={setCategory} />
+      <div className="tasks">
+        <h5>Tasks</h5>
+        <NewTaskForm  categories={CATEGORIES.filter((cat)=>cat !=="All")} onTaskFormSubmit={handleAddTask}/>
+      <TaskList onDeleteTask={handleDeleteTask} tasks={visisbleTasks} />
+    </div>
     </div>
   );
 }
